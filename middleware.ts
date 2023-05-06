46f8077f51541dd298db7b737ca28ef0d1c1f4ad
Minung Han <hmu332233@gmail.com>
2023-05-07 02:14:08 +0900

    미들웨어 예시용 주석 수정

--- middleware.ts
+++ middleware.ts
@@ -23,18 +23,6 @@ function getLocale(request: NextRequest) {
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname
 
-  // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
-  // // If you have one
-  if (
-    [
-      '/manifest.json',
-      '/favicon.ico',
-      // Your other files in `public`
-    ].includes(pathname)
-  ) {
-    return;
-  }
-    
   // Check if there is any supported locale in the pathname
   const pathnameIsMissingLocale = locales.every(
     (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
@@ -53,7 +41,7 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     // Skip all internal paths (_next)
-    // '/((?!_next).*)',
+    // '/((?!api|_next/static|_next/image|favicon.ico).*)'
     // Optional: only run on root (/) URL
     '/'
   ],
